641a9ab14373d50c92fedbc0e691c86180f30c2a
mahfuzar175 <mahfuzar175@gmail.com>
2023-11-07 02:15:32 +0600

    add a Manage route

--- src/Pages/My Services/MyServices.jsx
+++ src/Pages/My Services/MyServices.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
+import MyServiceCard from "./MyServiceCard";
 
 const MyServices = () => {
+
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/services")
+      .then((res) => res.json())
+      .then((data) => {
+        setServices(data);
+      });
+  }, []);
+
+
   return (
-    <div>
+    <div className="flex flex-col items-center pt-4 bg-gray-50">
       <Helmet>
-        <title>Travel Tandem | My Services</title>
+        <title>Travel Tandem | Services</title>
       </Helmet>
-      <h2>My Services</h2>
+      <div className="text-center mb-20">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {
+            services.map(service => <MyServiceCard  key={service._id} service={service}></MyServiceCard>)
+          } 
+        </div>
+      </div>
     </div>
   );
 };
